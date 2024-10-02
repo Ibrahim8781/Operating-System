233e0b6acd72e4332d12e480df30d4300e60a7b9
Ibrahim Ahmad <117165097+Ibrahim8781@users.noreply.github.com>
2024-10-02 17:41:03 +0000

    corrected some files removed bugs

--- 22I8781_Q1_server_monitor.sh
+++ 22I8781_Q1_server_monitor.sh
@@ -2,20 +2,31 @@
 
 LOG_FILE="./server.log"
 
-# disk_usage=$(df / | grep /)
+# disk_usage
+#            check for path | filer line | print 5th column | remove % sign
 disk_usage=$(df / | grep / | awk '{print $5}' | sed 's/%//')
 
-  echo "$(date): Disk usage is $disk_usage%" >> $LOG_FILE
+echo "$(date): Disk usage is $disk_usage%" >> $LOG_FILE
+
 if [ "$disk_usage" -gt 50 ]; then
   echo "Alert: Disk usage is above 50%!" >> $LOG_FILE
 else
     echo "Disk usage is normal." >> $LOG_FILE
 fi
 
-cpu_idle=$(top  -bn1 | grep "Cpu(s)" | awk '{print $8}' )
-cpu_usage=$(echo "100 - $cpu_idle" | bc)  
+# cpu_usage
+#  run top command data with one iteration  | filter line with "Cpu(s)" | print 8th column | remove signs to get numeric value
+cpu_idle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}' | sed 's/,/./')
 
-  echo "$(date): CPU usage is $cpu_usage%" >> $LOG_FILE
+#calculate cpu usage cpacity - idle cpu (free)
+cpu_usage=$(echo "100 - $cpu_idle" | bc)
+
+#
+echo "CPU idle: $cpu_idle" >> $LOG_FILE
+
+echo "$(date): CPU usage is $cpu_usage%" >> $LOG_FILE
+
+
 if (( $(echo "$cpu_usage > 50" | bc -l) )); then
   echo " Alert: CPU usage is above 50%!" >> $LOG_FILE
 else
@@ -23,8 +34,12 @@ else
 fi
 
 
+# memory_usage
+#  get total memory | get line with memory data | extract total memory
 total_memory=$(free -m | grep Mem | awk '{print $2}')
 available_memory=$(free -m | grep Mem | awk '{print $7}')
+# calculate memory usage in percentage
+
 memory_usage=$(echo "($available_memory / $total_memory) * 100" | bc -l)
 
   echo "$(date): Total memory is: $total_memory" >> $LOG_FILE
@@ -35,3 +50,4 @@ else
     echo "$(date): Available memory is: $available_memory" >> $LOG_FILE
     echo "$(date): Current available memory is: $memory_usage%" >> $LOG_FILE
 fi
+
---
+++ 22I8781_Q4.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+
+    printf("m: PID = %d (Root)\n", getpid());
+
+    pid_t pidM1 = fork();  
+
+    if (pidM1 < 0) {
+        printf("Fork failed\n");
+        return 1;
+    } else if (pidM1 == 0) {
+        // m1 lefot of m
+        printf("m1: PID = %d, m PID = %d\n", getpid(), getppid());
+
+        pid_t pidM2 = fork();  
+
+        if (pidM2 == 0) {
+            // Inside m2 lefot of m1
+            printf("m2: PID = %d, m1 PID = %d\n", getpid(), getppid());
+
+            // C5 C6 children of m2
+            pid_t pidC5 = fork();
+            if (pidC5 == 0) {
+                printf("C5: PID = %d, m2 PID = %d\n", getpid(), getppid());
+                
+                // C9 C10 children of C5
+                pid_t pidC9 = fork();
+                if (pidC9 == 0) {
+                    printf("C9: PID = %d, C5 PID = %d\n", getpid(), getppid());
+                    return 0;
+                }
+                wait(NULL);  
+
+                pid_t pidC10 = fork();
+                if (pidC10 == 0) {
+                    printf("C10: PID = %d, C5 PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+                return 0;  
+            }
+            wait(NULL);  
+
+            pid_t pidC6 = fork();
+            if (pidC6 == 0) {
+                printf("C6: PID = %d, m2 PID = %d\n", getpid(), getppid());
+                
+                // C11  C12 child C6
+                pid_t pidC11 = fork();
+                if (pidC11 == 0) {
+                    printf("C11: PID = %d, C6 PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+
+                pid_t pidC12 = fork();
+                if (pidC12 == 0) {
+                    printf("C12: PID = %d, C6 PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+                return 0;  
+            }
+            wait(NULL);  
+            return 0;  
+        }
+        wait(NULL);  
+
+        // right child of m1
+        pid_t pidC2 = fork();
+        if (pidC2 == 0) {
+            printf("C2: PID = %d, m1 PID = %d\n", getpid(), getppid());
+            
+            // C7, C8 child of C2
+            pid_t pidC7 = fork();
+            if (pidC7 == 0) {
+                printf("C7: PID = %d, C2 PID = %d\n", getpid(), getppid());
+                
+                // C13 C14 children of C7
+                pid_t pidC13 = fork();
+                if (pidC13 == 0) {
+                    printf("C13: PID = %d, C7 PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+
+                pid_t pidC14 = fork();
+                if (pidC14 == 0) {
+                    printf("C14: PID = %d, C7 PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+                return 0;  
+            }
+            wait(NULL);  
+
+            pid_t pidC8 = fork();
+            if (pidC8 == 0) {
+                printf("C8: PID = %d, C2 PID = %d\n", getpid(), getppid());
+                
+                // C15 C16 child of C8
+                pid_t pidC15 = fork();
+                if (pidC15 == 0) {
+                    printf("C15: PID = %d, C8 PID = %d\n", getpid(), getppid());
+                    return 0;
+                }
+                wait(NULL);  
+
+                pid_t pidC16 = fork();
+                if (pidC16 == 0) {
+                    printf("C16: PID = %d, C8 PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+                return 0;  
+            }
+            wait(NULL);  
+            return 0;  
+        }
+        wait(NULL);  
+        return 0;  
+    } else {
+        // root m 
+        pid_t pidC1 = fork();
+        if (pidC1 == 0) {
+            printf("C1: PID = %d, m PID = %d\n", getpid(), getppid());
+            
+            //right child of m
+            pid_t pidC3 = fork();
+            if (pidC3 == 0) {
+                printf("C3: PID = %d, C1 PID = %d\n", getpid(), getppid());
+                
+                // Create C17 and C18 as children of C3
+                pid_t pidC17 = fork();
+                if (pidC17 == 0) {
+                    printf("C17: PID = %d, C3 PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+
+                pid_t pidC18 = fork();
+                if (pidC18 == 0) {
+                    printf("C18: PID = %d, C3 PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+                return 0;  
+            }
+            wait(NULL);  
+
+            pid_t pidC4 = fork();
+            if (pidC4 == 0) {
+                printf("C4: PID = %d, C1 PID = %d\n", getpid(), getppid());
+                
+                // C19 child C4
+                pid_t pidC19 = fork();
+                if (pidC19 == 0) {
+                    printf("C19: PID = %d,  PID = %d\n", getpid(), getppid());
+                    return 0;  
+                }
+                wait(NULL);  
+                return 0;  
+            }
+            wait(NULL);  
+            return 0;  
+        }
+        wait(NULL);  
+    }
+
+    wait(NULL);  
+    return 0;
+}
